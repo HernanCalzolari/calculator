759bc3e8fdf8052a9f14a820df0f8753475d3eec
Hernan Calzolari <pigote@gmail.com>
2023-11-20 23:00:50 -0300

    Disabled = button on no operation, rounded to 2 decimals

--- script.js
+++ script.js
@@ -3,50 +3,66 @@
 let displayValue = '';
 
 function clearDisplay() {
-  displayValue = '';
-  updateDisplay();
-}
-
-function appendCharacter(character) {
-  displayValue += character;
-  updateDisplay();
-}
-
-function calculateResult() {
-  try {
-    const result = evaluateExpression(displayValue);
-    displayValue = result.toString();
+    displayValue = '';
     updateDisplay();
-  } catch (error) {
-    displayValue = 'Error';
-    updateDisplay();
+    updateEqualsButton();
   }
-}
+  
+  function appendCharacter(character) {
+    displayValue += character;
+    updateDisplay();
+    updateEqualsButton();
+  }
+  
+  function calculateResult() {
+    try {
+      const result = evaluateExpression(displayValue);
+      displayValue = result.toString();
+      updateDisplay();
+      updateEqualsButton();
+    } catch (error) {
+      displayValue = 'Error';
+      updateDisplay();
+      updateEqualsButton();
+    }
+  }
 
 function evaluateExpression(expression) {
-  // Implement a custom evaluation logic
-  const operators = ['+', '-', '*', '/'];
-  const values = expression.split(/[\+\-\*\/]/);
-  const operatorsAndValues = expression.split(/(\+|\-|\*|\/)/).filter(token => token.trim() !== '');
+  // Use a regular expression to split the expression into numbers and operators
+  const tokens = expression.match(/(\d+|\+|\-|\*|\/)/g);
+
+  if (!tokens) {
+    throw new Error('Invalid expression');
+  }
 
   // Perform multiplication and division first
-  for (const operator of ['*', '/']) {
-    for (let i = 0; i < operatorsAndValues.length; i++) {
-      if (operatorsAndValues[i] === operator) {
-        const result = operate(parseFloat(values[i]), parseFloat(values[i + 1]), operator);
-        values.splice(i, 2, result);
-        operatorsAndValues.splice(i, 1);
-        i--;
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i] === '*' || tokens[i] === '/') {
+      const operator = tokens[i];
+      const leftOperand = parseFloat(tokens[i - 1]);
+      const rightOperand = parseFloat(tokens[i + 1]);
+
+      if (isNaN(leftOperand) || isNaN(rightOperand)) {
+        throw new Error('Invalid operands');
       }
+
+      const result = operate(leftOperand, rightOperand, operator);
+      tokens.splice(i - 1, 3, result);
+      i--;
     }
   }
 
   // Perform addition and subtraction
-  let result = parseFloat(values[0]);
-  for (let i = 0; i < operatorsAndValues.length; i++) {
-    const operator = operatorsAndValues[i];
-    const nextValue = parseFloat(values[i + 1]);
-    result = operate(result, nextValue, operator);
+  let result = parseFloat(tokens[0]);
+  for (let i = 1; i < tokens.length; i += 2) {
+    const operator = tokens[i];
+    const operand = parseFloat(tokens[i + 1]);
+
+    if (isNaN(operand)) {
+      throw new Error('Invalid operand');
+    }
+
+    result = operate(result, operand, operator);
   }
 
   return result;
@@ -64,12 +80,29 @@ function operate(a, b, operator) {
       if (b === 0) {
         throw new Error('Division by zero');
       }
-      return a / b;
+      return roundToTwoDecimals(a / b);
     default:
       throw new Error('Invalid operator');
   }
 }
 
+function roundToTwoDecimals(value) {
+    // Round the value to two decimals
+    return Math.round(value * 100) / 100;
+  }
+
 function updateDisplay() {
   document.getElementById('display').value = displayValue;
 }
+
+function updateEqualsButton() {
+    // Enable or disable the equals button based on whether there is a valid operation to perform
+    const equalsButton = document.getElementById('equalsBtn');
+    equalsButton.disabled = !hasValidOperation();
+  }
+  
+  function hasValidOperation() {
+    // Check if the display value has a valid operation
+    const operatorsRegex = /[\+\-\*\/]/;
+    return operatorsRegex.test(displayValue);
+  }
